e3b52d2d613c87227f5906783d493144dc44b09f
tetlv3 <tetsuya@Tetsuya.local>
2019-02-11 00:01:18 -0700

    Ansibleをインストールするまでの不具合修正

--- InstallAnsible.sh
+++ InstallAnsible.sh
@@ -1,13 +1,32 @@
 #!/bin/sh -x
+
+which brew >/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+  echo "not found command brew"
+  echo "install brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+which wget >/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+  echo "not found command wget"
+  echo "install wget"
+  brew install wget
+fi
+
 which pip >/dev/null 2>&1
 if [ $? -ne 0 ];
 then
   echo "not found command pip"
   echo "install pip"
-  sudo easy_install pip
+  curl https://bootstrap.pypa.io/get-pip.py > ./get-pip.py
+  sudo python ./get-pip.py
+  rm ./get-pip.py
 fi
 
-which ansible >/dev/null 2>&1
+sudo which ansible >/dev/null 2>&1
 if [ $? -ne 0 ];
 then
   echo "not found command ansible"
