7611618ff4db1f14ab68200b663b784da9a44297
yakult1995 <destin.tax@outlook.jp>
2020-01-02 10:15:02 +0900

    :sparkles: Install ansible through brew

--- InstallAnsible.sh
+++ InstallAnsible.sh
@@ -8,28 +8,10 @@ then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-which wget >/dev/null 2>&1
-if [ $? -ne 0 ];
-then
-  echo "not found command wget"
-  echo "install wget"
-  brew install wget
-fi
-
-which pip >/dev/null 2>&1
-if [ $? -ne 0 ];
-then
-  echo "not found command pip"
-  echo "install pip"
-  curl https://bootstrap.pypa.io/get-pip.py > ./get-pip.py
-  sudo python ./get-pip.py
-  rm ./get-pip.py
-fi
-
 sudo which ansible >/dev/null 2>&1
 if [ $? -ne 0 ];
 then
   echo "not found command ansible"
   echo "install ansible"
-  sudo pip install ansible
+  sudo brew install ansible
 fi
